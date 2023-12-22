a4b9965777ed06ae105e5a96520128aaa7b415f1
lonperman <lonperman@gmail.com>
2023-12-22 12:18:59 -0500

    feat: deploy server

---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+const { register, login, setAvatar, getAllUsers, logOut } = require("../controllers/usersController");
+
+const router = require("express").Router();
+
+router.post("/register", register);
+router.post("/login", login);
+router.get("/logout/:id", logOut);
+router.post("/setAvatar/:id", setAvatar);
+router.get('/allusers/:id',getAllUsers)
+
+module.exports = router;
